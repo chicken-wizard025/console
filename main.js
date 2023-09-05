8150282711a47ab7ebbc91c89854521087e841d2
chicken-wizard025 <127834190+chicken-wizard025@users.noreply.github.com>
2023-09-05 18:48:16 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+var first_name="Ranbir"
+console.log("Firstname:"+first_name);
+
+var last_name="Kapoor"
+console.log("Lastname:",last_name);
+
+var fullname=first_name.concat(last_name)
+console.log("Full name:",fullname)
+var a = 6
+var b=24
+var add= a+b;
+console.log(add);
+var sub= b-a;
+console.log(sub);
+var mult= a*b;
+console.log(mult);
+var div= b/a;
+console.log(div);
